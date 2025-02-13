ce9a21cc608d0ec16b8f51abe22e530d4c2bad5c
Your Name <your.email@example.com>
2025-02-13 17:28:55 +0700

    Update: config

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ class Config:
     SECRET_KEY = 'Jzn3f5cxaqVYR6bmuyBZ9h4gAFrjLQPUwHM7XTNKskvedWtSCp'
     JWT_SECRET_KEY = 'xDs8CQZNuMXb2PvkyqLYra9T3EAUmzwBdfh7n4KtFS6eRjWGcg'  # Thêm khóa bí mật cho JWT
     AVAILABLE_PERMISSIONS = ["Dashboard", "Users", "Vai Trò"]
+        # Dành cho lúc dev:
+        # chạy cái này trước cloudflared access tcp --hostname mysql.dannycode.site --url localhost:5051
+        # Start-Process -FilePath "cloudflared.exe" -ArgumentList "access tcp --hostname mysql.dannycode.site --url localhost:5051" -WindowStyle Hidden
+        # Chạy xong sẽ liên kết với server thông qua tunnel của cloudflare => có thể connect từ xa và dùng ip là 127.0.0.1:5051
+        # Stop-Process -Name "cloudflared"

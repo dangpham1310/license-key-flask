b0e5ea7048bfb42a66b55bb35bad2c0cebc3312f
Your Name <your.email@example.com>
2025-02-14 11:17:17 +0700

    Update: LogsHistory

---
+++ migrations/versions/641b06e6d6ae_recreating_lost_migration.py
@@ -0,0 +1,24 @@
+"""Recreating lost migration
+
+Revision ID: 641b06e6d6ae
+Revises: da26574f1e7b
+Create Date: 2025-02-14 11:14:30.525668
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '641b06e6d6ae'
+down_revision = 'da26574f1e7b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
